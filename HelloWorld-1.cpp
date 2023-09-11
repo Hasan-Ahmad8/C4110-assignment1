fe30a623b213d26a8498c9668952a27dc4692695
Hasan <Hasanahmad4540@gmail.com>
2023-09-11 15:37:00 -0400

    Resolved merge conflict by incorperating both suggestions.

--- HelloWorld-1.cpp
+++ HelloWorld-1.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Hasan Ahmad <hl5846@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
